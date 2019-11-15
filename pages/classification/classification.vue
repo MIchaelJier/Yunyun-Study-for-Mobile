c49dbd2ff62c398be1ed3c26dca304cfff03826f
陈英杰 <chem908430737@icloud.com>
2019-11-15 09:58:13 +0800

    完成分类页 tab和tip

--- pages/classification/classification.vue
+++ pages/classification/classification.vue
@@ -20,6 +20,11 @@
 			<template v-slot:content>
 				<!-- 轮播图 -->
 				<!-- tips-->
+				<view class="content-tips">
+						<block v-for="tip in tabClass[nowSelect].children" :key="tip.id">
+							<view class="content-tip">{{ tip.name }}</view>
+						</block>
+				</view>
 				<!-- 分类 -->
 			</template>
 		</better-sticky>
@@ -33,25 +38,76 @@
 				tabClass:[
 					{
 						id:0,
-						name:"职场提升"
+						name:"职场提升",
+						children:[
+							{id: 1, name: "办公软件"},
+							{id: 2, name: "效率工具"},
+							{id: 3, name: "电脑基础"},
+							{id: 4, name: "Excel"},
+							{id: 5, name: "职场能力"},
+							{id: 6, name: "商业管理"},
+							{id: 7, name: "专业岗位"},
+						]
 					},{
 						id:1,
-						name:"编程与开发"
+						name:"编程与开发",
+						children:[
+							{id: 1, name: "编程语言"},
+							{id: 2, name: "前端开发"},
+							{id: 3, name: "后端开发"},
+							{id: 4, name: "移动开发"},
+							{id: 5, name: "网络与安全"},
+						]
 					},{
 						id:2,
-						name:"AI/数据科学"
+						name:"AI/数据科学",
+						children:[
+							{id: 1, name: "编程语言"},
+							{id: 2, name: "前端开发"},
+							{id: 3, name: "后端开发"},
+							{id: 4, name: "移动开发"},
+							{id: 5, name: "网络与安全"},
+						]
 					},{
 						id:3,
-						name:"产品与运营"
+						name:"产品与运营",
+						children:[
+							{id: 1, name: "编程语言"},
+							{id: 2, name: "前端开发"},
+							{id: 3, name: "后端开发"},
+							{id: 4, name: "移动开发"},
+							{id: 5, name: "网络与安全"},
+						]
 					},{
 						id:4,
-						name:"设计创意"
+						name:"设计创意",
+						children:[
+							{id: 1, name: "编程语言"},
+							{id: 2, name: "前端开发"},
+							{id: 3, name: "后端开发"},
+							{id: 4, name: "移动开发"},
+							{id: 5, name: "网络与安全"},
+						]
 					},{
 						id:5,
-						name:"电子商务"
+						name:"电子商务",
+						children:[
+							{id: 1, name: "编程语言"},
+							{id: 2, name: "前端开发"},
+							{id: 3, name: "后端开发"},
+							{id: 4, name: "移动开发"},
+							{id: 5, name: "网络与安全"},
+						]
 					},{
 						id:6,
-						name:"语言学习"
+						name:"语言学习",
+						children:[
+							{id: 1, name: "编程语言"},
+							{id: 2, name: "前端开发"},
+							{id: 3, name: "后端开发"},
+							{id: 4, name: "移动开发"},
+							{id: 5, name: "网络与安全"},
+						]
 					}
 				],
 				nowSelect:0,
@@ -66,13 +122,11 @@
 				this.nowSelect = id;
 			}
 		},
-		onReady() {
-			this.tabMid(this.nowSelect);
-		},
 		onPageScroll(e) {
 			this.scrollTop= e.scrollTop
 		},
 		methods: {
+			//点击tab选中项目
 			chooseItem:function(id){
 				this.nowSelect = id;
 			},
@@ -91,22 +145,31 @@
 							}
 					});
 			},
+			//tab条选中居中
 			tabMid:function(id){
-				let that = this;
-				let item = uni.createSelectorQuery().selectAll('.tab-item');
-				item.fields({
-				  size: true,
-				  scrollOffset: true
-				}, data => {
-				  // console.log("得到节点信息" + JSON.stringify(data));
-				  let distance = 0;
-				  data.forEach(function(arr,index){
-					  if(index < id)
-						distance += arr.width;
-					});
-					//偏移量 = 节点到srcoll-view起点长度 - 屏幕宽度/2 + 节点长度/2
-				  that.scrollLeft = distance - that.$ww/2 + data[id].width/2
-				}).exec();
+				this.$nextTick(function () { //防止第一次进入scrollLeft无效
+					let that = this;
+					//改变页面标题名字
+					let title =  that.tabClass.filter( i => i.id === that.nowSelect)[0].name;
+					uni.setNavigationBarTitle({
+					　　title: title
+					})
+					//设置srcoll-view srcollTop值
+					const item = uni.createSelectorQuery().selectAll('.tab-item');
+					item.fields({
+					  size: true,
+					  scrollOffset: true
+					}, data => {
+					  // console.log("得到节点信息" + JSON.stringify(data));
+					  let distance = 0;
+					  data.forEach(function(arr,index){
+						  if(index < id)
+							distance += arr.width;
+						});
+						//偏移量 = 节点到srcoll-view起点长度 - 屏幕宽度/2 + 节点长度/2
+					  that.scrollLeft = distance - that.$ww/2 + data[id].width/2
+					}).exec();
+				})
 			}
 		},
 		watch:{
