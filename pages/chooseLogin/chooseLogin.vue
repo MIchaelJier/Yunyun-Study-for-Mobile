d94fa37d417e8e5e1c13c24e8f0a168505482058
MIchaelJier <chem908430737@icloud.com>
2020-02-01 22:16:00 +0800

    增加html模版index.js，引入qq jssdk，开始 写 H5端 QQ登录操作

--- pages/chooseLogin/chooseLogin.vue
+++ pages/chooseLogin/chooseLogin.vue
@@ -20,20 +20,22 @@
 					<view class="line"></view>
 				</view>
 				<view class="bottom-icons bottom-title">
+					<!-- 在 H5端 和 App端 可以通过微信、QQ登录 -->
 					<!-- #ifdef H5 || APP-PLUS -->
-					<view class="bottom-icon">
+					<view class="bottom-icon" data-logintype="qq" @click="appOAuthLogin">
 						<view class="bottom-icon-o" hover-class="btn-hover">
 							<uni-icons type="qq" size="46" :color="iconColor"></uni-icons>
 						</view>
 						<text class="bottom-icon-t">QQ</text>
 					</view>
-					<view class="bottom-icon">
+					<view class="bottom-icon" data-logintype="weixin" @click="appOAuthLogin">
 						<view class="bottom-icon-o" hover-class="btn-hover">
 							<uni-icons type="weixin" size="46" :color="iconColor"></uni-icons>
 						</view>
 						<text class="bottom-icon-t">微信</text>
 					</view>
 					<!-- #endif -->
+					<!-- 在 微信小程序端 只能通过微信登录 -->
 					<!-- #ifdef MP-WEIXIN -->
 					<button class="bottom-icon" open-type="getUserInfo" hover-class="none" >
 						<view class="bottom-icon-o" hover-class="btn-hover">
@@ -47,7 +49,6 @@
 		</view>
 	</view>
 </template>
-
 <script>
 	export default {
 		data() {
@@ -56,7 +57,24 @@
 			}
 		},
 		methods: {
-			
+			appOAuthLogin(e){
+				const provider = e.currentTarget.dataset.loginType;
+				// #ifdef APP-PLUS
+				uni.login({
+					provider,
+					success(res) {
+						console.log(res)
+					}
+				})
+				// #endif
+				
+				// #ifdef H5
+				QC.Login.showPopup({
+				     appId:"101825637",
+					 redirectURI:"https:zfroot.top:8081/yun/auth"
+				});
+				// #endif
+			}
 		}
 	}
 </script>
